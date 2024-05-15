a64ceffbf9f08f79bbd9cb0d2e778f4bb28a4c2a
dheerajprakria <dheeraj@prakria.com>
2024-05-15 16:04:35 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
           type="image/x-icon"
           sizes="16x16"
         />
-        <title>Prakria Tech - Description</title>
+        <title>Prakria Tech - Top Mobile App Development Company l PRAKRIA</title>
       </head>
       <body>
         <ScrollProgress />
