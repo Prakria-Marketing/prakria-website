f13dc07df1383d2208cce01e45fcd610f03ccbdb
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air.local>
2025-09-17 14:45:55 +0530

    add gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ import ScrollProgress from "@/components/common/ScrollProgress";
 import ThemeSwitch from "@/components/common/Theme";
 import useWow from "@/hooks/useWow";
 import Whatsapp from "./whatsapp/Whatsapp";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -45,8 +46,43 @@ export default function RootLayout({ children }) {
           sizes="16x16"
         />
         <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KMGMRW7H');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMGMRW7H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TDS9XMGZG2"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TDS9XMGZG2');
+          `}
+        </Script>
         <Whatsapp />
         <ScrollProgress />
         <ThemeSwitch />
